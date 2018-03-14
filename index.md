9cd19cd1dc5fae590286135110562318b47da8e0
Victoria Clark <victoria.clark@Victorias-MBP.zyxel.com>
2018-03-13 20:58:02 -0400

    blah

--- index.md
+++ index.md
@@ -4,3 +4,13 @@
 # See: https://jekyllrb.com/docs/themes/#overriding-theme-defaults
 layout: home
 ---
+<html>
+<head>
+</head>
+<body>
+
+<script src='javascript/d3.js'></script>
+<script src='javascript/lodash.js'></script>
+<script src='javascript/analysis.js'></script>
+</body>
+</html>
---
+++ javascript/analysis.js
@@ -0,0 +1,17 @@
+    d3.json("http://search.lib.virginia.edu/catalog/u7004813.json", function(data) {
+      console.log(data['response']['docs'][0]);
+
+      // the code you're looking for
+      var needle = 'the';
+
+      // iterate over each element in the array
+      for (var i = 0; i < data.length; i++){
+        // look for the entry with a matching `code` value
+        console.log(data['response']['docs'][i])
+        if (data['response']['docs'][i].includes(needle)){
+           // we found it
+          // obj[i].name is the matched result
+          console.log('success');
+        }
+      }
+    });
